eafd43e276abda2c122e5a91e1e030b7e6930873
syou4 <g1444693@gmail.com>
2018-06-12 17:43:15 +0900

    edit Gemfile

--- command.rb
+++ command.rb
@@ -6,18 +6,20 @@ ENV['TZ'] = 'JST-9' # Timezone
 
 post '/slack/command' do
     text = params['text']
-    if text =~ /何時/
-        now = Time.now
-        responseText = "今は#{now.hour}時#{now.min}分#{now.sec}秒です。"
-    elsif text =~ /今日/
-        today = Time.now
-        responseText = "今日は#{today.year}年#{today.month}月#{today.day}日です。"
-    elsif text =~ /明日/
-        tomorrow = Time.now + (60*60*24)
-        responseText = "明日は#{tomorrow.year}年#{tomorrow.month}月#{tomorrow.day}日です。"
-    else
-        responseText = "例: 何時 / 今日 / 明日"
-    end
+
+    # if text =~ /何時/
+    #     now = Time.now
+    #     responseText = "今は#{now.hour}時#{now.min}分#{now.sec}秒です。"
+    # elsif text =~ /今日/
+    #     today = Time.now
+    #     responseText = "今日は#{today.year}年#{today.month}月#{today.day}日です。"
+    # elsif text =~ /明日/
+    #     tomorrow = Time.now + (60*60*24)
+    #     responseText = "明日は#{tomorrow.year}年#{tomorrow.month}月#{tomorrow.day}日です。"
+    # else
+    #     responseText = "例: 何時 / 今日 / 明日"
+    # end
+    responseText=text
     json({
         text: responseText,
         response_type: 'in_channel',
